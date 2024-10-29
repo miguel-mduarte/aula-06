6ef1af8ee76d84fb0ae28016a4658649d57519a3
Miguel M <116860787+miguel-mduarte@users.noreply.github.com>
2024-10-29 19:17:09 -0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 3 . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. e e 4 e e 4 e e e 4 4 e 4 e . 
+e 4 f f f f f f f f f f f f 4 e 
+e f f f f f f f f f f f f f f e 
+e e f f f f f f f f f f f f 4 e 
+e e f f f f f f f f f f f f 4 e 
+e 4 e e e e 4 e e e e e e e 4 e 
+e 4 4 4 e e 4 e 4 e 4 4 4 4 4 e 
+e 4 e 4 e e 4 e 4 e 4 4 e 4 e e 
+e 4 e 4 e e 4 4 4 e 4 4 e 4 e e 
+e 4 e 4 e e 4 4 4 e 4 4 e 4 e e 
+e 4 e 4 e e 4 e 4 4 4 4 e 4 4 e 
+e e e 4 4 4 4 e 4 4 4 4 4 e e e 
+. . e e e e e e e e e e e e . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
